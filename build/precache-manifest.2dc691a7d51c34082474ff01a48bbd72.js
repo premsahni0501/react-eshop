eceddbc6ea5bd45869b8103b858a7264265385dc
Prem Chand <premsahni0501@gmail.com>
2019-05-29 20:03:45 +0530

    gh-pages deploy

---
+++ build/precache-manifest.2dc691a7d51c34082474ff01a48bbd72.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a846aad3045896e4d5bc5fd83ccf8be",
+    "url": "/react-eshop/index.html"
+  },
+  {
+    "revision": "a5f0c8aa5512eef54a99",
+    "url": "/react-eshop/static/css/main.7781f61f.chunk.css"
+  },
+  {
+    "revision": "be39e08a0c0007a66ca7",
+    "url": "/react-eshop/static/js/2.ec78b424.chunk.js"
+  },
+  {
+    "revision": "a5f0c8aa5512eef54a99",
+    "url": "/react-eshop/static/js/main.93c5f6e4.chunk.js"
+  },
+  {
+    "revision": "fd7bb2f9ebd1846db376",
+    "url": "/react-eshop/static/js/runtime~main.b736abdc.js"
+  },
+  {
+    "revision": "3debe0242b68ad815ff7c4d286f94325",
+    "url": "/react-eshop/static/media/1.3debe024.png"
+  },
+  {
+    "revision": "e1cb01131367d995eef48b54f37dbd26",
+    "url": "/react-eshop/static/media/2.e1cb0113.jpg"
+  },
+  {
+    "revision": "51acdfb7cfa11c91a92322ea3e1ec8be",
+    "url": "/react-eshop/static/media/3.51acdfb7.jpg"
+  },
+  {
+    "revision": "f4f663d7977f1642009b1ee69d8fff97",
+    "url": "/react-eshop/static/media/3.f4f663d7.png"
+  },
+  {
+    "revision": "896efbb87170f40f219f5e56e3d375f8",
+    "url": "/react-eshop/static/media/4.896efbb8.png"
+  },
+  {
+    "revision": "e4e782f6dde6a13c77c1e6aa72c69c3a",
+    "url": "/react-eshop/static/media/5.e4e782f6.png"
+  },
+  {
+    "revision": "5cf6c680685533d93d4a30ee294727e6",
+    "url": "/react-eshop/static/media/nav_toggle.5cf6c680.svg"
+  },
+  {
+    "revision": "d8da81ad6e5b48c7b52f34b15f6614dc",
+    "url": "/react-eshop/static/media/search.d8da81ad.svg"
+  },
+  {
+    "revision": "1d80b9644f16a331ffa66647916fc42f",
+    "url": "/react-eshop/static/media/shopping_bag.1d80b964.svg"
+  }
+]);
